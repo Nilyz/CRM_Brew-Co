130a08b762c99d312cdd80d5b1d9825256947245
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2024-11-25 08:41:09 +0100

    Cambiado la organización de la customerScreen

--- app/src/main/java/com/example/brewco/ui/screens/customer/customerScreen.kt
+++ app/src/main/java/com/example/brewco/ui/screens/customer/customerScreen.kt
@@ -2,9 +2,12 @@ package com.example.brewco.ui.screens.customer
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
@@ -20,6 +23,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.brewco.ui.components.CustomBottomNavBar
 import com.example.brewco.ui.components.CustomDrawer
@@ -83,19 +87,94 @@ fun CustomerScreen(navHostController: NavHostController) {
 
 @Composable
 fun CustomerItem(index: Int) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        elevation = CardDefaults.cardElevation(4.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(0xFFF0F0F0))
-    ) {
-        Text(
-            text = "Evento $index",
+        Row(
             modifier = Modifier
-                .padding(16.dp),
+                .fillMaxWidth()
+                .padding(8.dp),
+            //horizontalArrangement = Arrangement.Center
+        ) {
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth(0.50f)
+                    .height(300.dp)
+                    .padding(8.dp)
+            ){
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth(0.35f)
+                        .height(300.dp)
+                        .padding(8.dp),
+                    elevation = CardDefaults.cardElevation(4.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFF0F0F0))
+                ) {
+                    Text(
+                        text = "Evento $index",
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .padding(16.dp),
+                    )
+                    Text(
+                        text = "Puntos: 250",
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(8.dp),
+                    )
+                    Text(
+                        text = "Teléfono: 123456789",
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(8.dp),
+                    )
+                    Text(
+                        text = "Última compra: 12/12/2021",
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(8.dp),
+                    )
+                }
 
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.50f)
+                    .height(300.dp)
+                    .padding(8.dp)
+            ) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth(0.35f)
+                        .height(300.dp)
+                        .padding(8.dp),
+                    elevation = CardDefaults.cardElevation(4.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFF0F0F0))
+                ) {
+                    Text(
+                        text = "Evento $index",
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .padding(16.dp),
+                    )
+                    Text(
+                        text = "Puntos: 250",
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(8.dp),
+                    )
+                    Text(
+                        text = "Teléfono: 123456789",
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(8.dp),
+                    )
+                    Text(
+                        text = "Última compra: 12/12/2021",
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(8.dp),
+                    )
+                }
 
-            )
-    }
+            }
+        }
+
 }
