ce50707fb3d617471703afac146af0dc3595e8d7
Hugo Sanz <148755800+ggdsrll@users.noreply.github.com>
2024-11-28 18:30:36 +0100

    Updated The Style of the Costumer Details Screen

--- app/src/main/java/com/example/brewco/ui/screens/customer/customerDetailsScreen.kt
+++ app/src/main/java/com/example/brewco/ui/screens/customer/customerDetailsScreen.kt
@@ -1,20 +1,37 @@
 package com.example.brewco.ui.screens.customer
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import com.example.brewco.data.model.Client
+import com.example.brewco.ui.theme.Brown
+import com.example.brewco.ui.theme.*
 
 @Composable
 fun CustomerDetailsScreen(
@@ -24,28 +41,118 @@ fun CustomerDetailsScreen(
 ) {
     var client by remember { mutableStateOf<Client?>(null) }
 
-    // Cargar el cliente desde firebase según la ID del que has clickeado
+    // Cargar el cliente desde Firebase
     LaunchedEffect(clientId) {
         viewModel.getClienteById(clientId) { fetchedClient ->
-            client = fetchedClient // Asigna el cliente recuperado
+            client = fetchedClient
         }
     }
 
     client?.let { cliente ->
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
+                .fillMaxWidth()
+                .padding(16.dp), // Padding general para toda la pantalla
+            verticalArrangement = Arrangement.spacedBy(24.dp) // Espaciado entre la información principal y las notas
         ) {
-            Text(
-                text = "Detalles del Cliente",
-            )
-            Text(text = "Nombre: ${cliente.nombre} ${cliente.apellido}")
-            Text(text = "Teléfono: ${cliente.telefono}")
-            Text(text = "Puntos: ${cliente.puntos}")
-            Text(text = "Última Compra: ${cliente.ultCompra}")
-            Text(text = "Notas: ${cliente.notas}")
+            // Contenedor principal de la información del cliente
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp) // Padding interno para centrar visualmente el contenido
+                    .align(Alignment.CenterHorizontally), // Centrar horizontalmente
+                verticalArrangement = Arrangement.spacedBy(12.dp), // Espaciado entre filas
+                horizontalAlignment = Alignment.CenterHorizontally // Asegura que todo el contenido quede centrado horizontalmente
+            ) {
+                // Nombre del cliente
+                Text(
+                    text = "${cliente.nombre} ${cliente.apellido}",
+                    fontWeight = FontWeight.Bold,
+                    color = Brown
+                )
+
+                // Teléfono
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(Icons.Default.Phone, contentDescription = "Teléfono", tint = Brown)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        text = cliente.telefono,
+                        fontWeight = FontWeight.Normal,
+                        color = Brown
+                    )
+                }
+
+                // Última compra
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(Icons.Default.DateRange, contentDescription = "Última compra", tint = Brown)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        text = "Última compra: ${cliente.ultCompra}",
+                        fontWeight = FontWeight.Normal,
+                        color = Brown
+                    )
+                }
+
+                // Correo electrónico
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(Icons.Default.Email, contentDescription = "Correo", tint = Brown)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        text = "Correo: ${cliente.correo}",
+                        fontWeight = FontWeight.Normal,
+                        color = Brown
+                    )
+                }
+
+                // Puntos
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        Icons.Default.CheckCircle, contentDescription = "Agregar",
+                        modifier = Modifier.size(20.dp),
+                        tint = Brown
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        text = "Puntos: ${cliente.puntos}",
+                        fontWeight = FontWeight.Bold,
+                        color = Brown
+                    )
+                    Icon(
+                        Icons.Default.Star, contentDescription = "Agregar",
+                        modifier = Modifier.size(20.dp),
+                        tint = Color.Yellow
+                    )
+                }
+            }
+
+            // Sección de notas
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Notas:",
+                    fontWeight = FontWeight.Bold,
+                    color = Brown
+                )
+                Text(
+                    text = "${cliente.notas}",
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Start,
+                    color = Color.Black
+                )
+            }
         }
     }
-
 }
